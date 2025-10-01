a2ad4ab4b2d49d3d57e8383e74ff3e240d1c0a2c
K'UK' ulkan <frankanya60@gmail.com>
2025-10-01 06:07:32 -0700

    new

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,13 +10,21 @@ async function main() {
   await prisma.user.upsert({
     where: { email: "admin@example.com" },
     update: {},
-    create: { email: "admin@example.com", passwordHash: adminPass, role: "admin" }
+    create: {
+      email: "admin@example.com",
+      password: adminPass,
+      role: "admin"
+    }
   });
 
   await prisma.user.upsert({
     where: { email: "user@example.com" },
     update: {},
-    create: { email: "user@example.com", passwordHash: userPass, role: "user" }
+    create: {
+      email: "user@example.com",
+      password: userPass,
+      role: "user"
+    }
   });
 }
 
